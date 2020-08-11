aeae60bd73729edb15b7d73b00bd670161fd8f99
Md Robaiatul Islam <imkuttumiah@gmail.com>
2020-08-11 16:46:20 +0600

    Bind token generation to form submit.

--- src/views/captchav3.blade.php
+++ src/views/captchav3.blade.php
@@ -1,13 +1,21 @@
 <?php
-    $input_field_id = 'recaptcha-token-' . \Illuminate\Support\Str::random(10);
+    $random = \Illuminate\Support\Str::random(10);
+    $input_field_id = 'recaptcha-token-' . $random;
 ?>
 
 <input type="hidden" id="{{ $input_field_id }}" class="g-recaptcha-response" name="g-recaptcha-response" value="" required>
 <script src="https://www.google.com/recaptcha/api.js?render={{ $public_key }}"></script>
 <script>
-    grecaptcha.ready(function() {
-        grecaptcha.execute("{{ $public_key }}", {action: "homepage"}).then(function(token) {
-            document.getElementById("{{ $input_field_id }}").value = token;
+    var {{ 'recaptcha_token_' . $random }} = document.getElementById("{{ $input_field_id }}");
+    var {{ 'form_' . $random }} = {{ 'recaptcha_token_' . $random }}.closest("form");
+
+    {{ 'form_' . $random }}.onsubmit = function(event) {
+        event.preventDefault();
+        grecaptcha.ready(function() {
+            grecaptcha.execute("{{ $public_key }}", {action: "submit"}).then(function(token) {
+                {{ 'recaptcha_token_' . $random }}.value = token;
+                {{ 'form_' . $random }}.submit();
+            });
         });
-    });
+    }
 </script>
